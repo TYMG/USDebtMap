77989908376ba8f57587b3f200a65d3692474bd0
Matthew Green <verde.mateo.a@gmail.com>
2017-09-20 23:27:02 -0400

    3/3 Unit Test Working. Style/Conent Header Footer

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,32 +1,39 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('Component: App', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+     fixture = TestBed.createComponent(AppComponent);
+      component = fixture.debugElement.componentInstance;
+
   }));
 
-  it('should create the app', async(() => {
+  it('should create the appComponent', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it(`should render p in the header`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const compiled = fixture.debugElement.nativeElement; //Use nativeElement to query the DOM
+    console.log(compiled.querySelector('header.test p'));
+    expect(compiled.querySelector('header.test p').textContent).toContain('Welcome to app!');
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  it('should render p in the body', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    const compiled = fixture.debugElement.nativeElement; //Use nativeElement to query the DOM
+    console.log(compiled.querySelector('body.app__main p'));
+    expect(compiled.querySelector('body.app__main p').textContent).toContain('This is the body');
   }));
 });

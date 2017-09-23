649d45e0050ceda67698b426abd4f6a11e76ac44
Matthew Green <verde.mateo.a@gmail.com>
2017-09-22 23:58:28 -0400

    Completed Building Base App

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -22,18 +22,19 @@ describe('Component: App', () => {
     expect(component).toBeTruthy();
   }));
 
-  it(`should render p in the header`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const compiled = fixture.debugElement.nativeElement; //Use nativeElement to query the DOM
-    console.log(compiled.querySelector('header.test p'));
-    expect(compiled.querySelector('header.test p').textContent).toContain('Welcome to app!');
-  }));
-
   it('should render p in the body', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement; //Use nativeElement to query the DOM
-    console.log(compiled.querySelector('body.app__main p'));
-    expect(compiled.querySelector('body.app__main p').textContent).toContain('This is the body');
+    console.log(compiled.querySelector('div.app__main p'));
+    expect(compiled.querySelector('div.app__main p').textContent).toContain('This is the body');
+  }));
+
+  it('should render p in the footer', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement; //Use nativeElement to query the DOM
+    console.log(compiled.querySelector('footer__text'));
+    expect(compiled.querySelector('.footer__text').textContent).toContain('Place sticky footer content here.'); 
   }));
 });
